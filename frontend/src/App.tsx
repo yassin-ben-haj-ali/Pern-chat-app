f289effb0d6422c7fe63248a55f662c178c28235
yassin-ben-haj-ali <yassinbenhajali5@gmail.com>
2024-10-09 14:47:18 +0100

    AuthContext setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,20 @@
-import { Route, Routes } from "react-router-dom"
+import { Navigate, Route, Routes } from "react-router-dom"
 import Login from "./pages/Login"
 import SignUp from "./pages/SignUp"
 import Home from "./pages/Home"
+import { useAuthContext } from "./context/AuthContext"
 
 function App() {
+
+  const { AuthUser } = useAuthContext();
 
   return (
     <div className="p-4 h-screen flex items-center justify-center">
 
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
+        <Route path="/" element={AuthUser ? <Home /> : <Navigate to='/login' />} />
+        <Route path="/login" element={!AuthUser ? <Login /> : <Navigate to='/' />} />
+        <Route path="/signup" element={!AuthUser ? <SignUp /> : <Navigate to='/' />} />
       </Routes>
 
     </div>
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import { AuthContextProvider } from './context/AuthContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <AuthContextProvider>
+        <App />
+      </AuthContextProvider>
     </BrowserRouter>
   </StrictMode>,
 )
